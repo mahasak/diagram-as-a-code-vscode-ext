35b21b2269136427bff950da25cb298de616971e
Max Pijittum <mpijittum@fb.com>
2021-02-07 20:22:33 +0800

    Extract method to main context

--- src/extension.ts
+++ src/extension.ts
@@ -4,8 +4,46 @@ import * as fs from 'fs'
 
 export function activate(context: vscode.ExtensionContext) {
 	const registerCommand = vscode.commands.registerCommand;
+	const isValidFileExtension = () => path.extname(getDiagramSource()) === '.py';
 
-	let command = registerCommand('diagramascodeviewer.start', () => {
+	const getDiagramSource = () => vscode.window.activeTextEditor?.document.uri.path ?? "";
+
+	const genDiagram = async (panel: vscode.WebviewPanel) => {
+		if (!isValidFileExtension()) {
+			return;
+		}
+
+		const proc = require('child_process');
+		const full_path = getDiagramSource();
+		const cmd = `cat ${full_path} | docker run -i --rm -v ${context.extensionPath}/media/out:/out gtramontina/diagrams:0.18.0`;
+		console.log(cmd);
+
+		proc.exec(cmd, (err: string, stdout: string, stderr: string) => {
+			if (err) {
+				console.log('error: ' + err);
+				return;
+			}
+			panel.webview.html = getContent();
+		});
+	}
+
+	const getContent = () => {
+		const full_path = getDiagramSource();
+		const target = path.basename(full_path ?? "", ".py");
+		const target_file = path.join(context.extensionPath, "media", "out", `${target}.png`);
+		var data = fs.readFileSync(target_file).toString('base64');
+
+		const content = `<!DOCTYPE html>
+			<html>
+			  <body>
+				<img src="data:image/png;base64, ${data}" >
+			  </body>
+			</html>`;
+
+		return content;
+	}
+
+	const command = registerCommand('diagramascodeviewer.start', () => {
 		const _disposables: vscode.Disposable[] = [];
 
 		const panel = vscode.window.createWebviewPanel(
@@ -17,50 +55,13 @@ export function activate(context: vscode.ExtensionContext) {
 			}
 		);
 
-		const isValidFileExtension = () => path.extname(getDiagramSource()) === '.py';
 
-		const getDiagramSource = () => vscode.window.activeTextEditor?.document.uri.path ?? "";
-
-		const  genDiagram = async () => {
-			if(!isValidFileExtension()) {
-				return;
-			}
-
-			const proc = require('child_process');
-			const full_path = getDiagramSource();
-			const cmd = `cat ${full_path} | docker run -i --rm -v ${context.extensionPath}/media/out:/out gtramontina/diagrams:0.18.0`;
-			console.log(cmd);
-			
-			proc.exec(cmd, (err: string, stdout: string, stderr: string) => {
-				if (err) {
-					console.log('error: ' + err);
-					return;
-				} 
-				panel.webview.html = getContent();
-			});
-		}
-
-		const getContent = () => {
-			const full_path = getDiagramSource();
-			const target = path.basename(full_path ?? "", ".py");
-			const target_file = path.join(context.extensionPath, "media", "out", `${target}.png`);
-			var data = fs.readFileSync(target_file).toString('base64');
-			
-			const content = `<!DOCTYPE html>
-			<html>
-			  <body>
-				<img src="data:image/png;base64, ${data}" >
-			  </body>
-			</html>`;
-
-			return content;
-		}
 
 		vscode.workspace.onDidSaveTextDocument(
 			(e) => {
 				console.log("Source diagram changed");
 				if (e.fileName === vscode.window.activeTextEditor?.document.fileName) {
-					genDiagram();
+					genDiagram(panel);
 				} else {
 					console.log("Source diagram not matched");
 				}
@@ -83,8 +84,8 @@ export function activate(context: vscode.ExtensionContext) {
 			null,
 			context.subscriptions
 		);
-		
-		genDiagram();
+
+		genDiagram(panel);
 	});
 	context.subscriptions.push(command);
 
